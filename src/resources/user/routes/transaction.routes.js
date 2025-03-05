a31d7174517d515ea416d2a5eed927be198c6139
FredAbod <fredrickbolutife@gmail.com>
2025-03-05 16:06:50 +0100

    getUserTransactions endpoint

--- src/resources/user/routes/transaction.routes.js
+++ src/resources/user/routes/transaction.routes.js
@@ -1,14 +1,17 @@
 import express from "express";
-import { deposit, transfer, withdraw } from "../controllers/transaction.controller.js";
+import { deposit, transfer, withdraw, getUserTransactions } from "../controllers/transaction.controller.js";
 import { flutterwaveDeposit } from "../controllers/flutterwaveTransaction.js";
 // import { auth } from "../../../middleware/auth.js";
 
 const router = express.Router();
 
 // Protected transaction routes
-router.post("/deposit",  deposit);
-router.post("/withdraw",  withdraw);
-router.post("/transfer",  transfer);
+router.post("/deposit", deposit);
+router.post("/withdraw", withdraw);
+router.post("/transfer", transfer);
+
+// Get user transactions
+router.get("/user/:userId", getUserTransactions);
 
 // Flutterwave webhook route (no auth needed as it's called by Flutterwave)
 router.post("/flutterwave-webhook", flutterwaveDeposit);
