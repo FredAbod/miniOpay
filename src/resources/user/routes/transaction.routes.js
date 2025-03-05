02b07ee1900780b885a3e5a12465da2ce3e62fcc
FredAbod <fredrickbolutife@gmail.com>
2025-03-05 12:11:41 +0100

    bug fixes

--- src/resources/user/routes/transaction.routes.js
+++ src/resources/user/routes/transaction.routes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { deposit, transfer, withdraw } from "../controllers/transaction.controller.js";
 import { flutterwaveDeposit } from "../controllers/flutterwaveTransaction.js";
-import { auth } from "../../../middleware/auth.js";
+// import { auth } from "../../../middleware/auth.js";
 
 const router = express.Router();
 
 // Protected transaction routes
-router.post("/deposit", auth, deposit);
-router.post("/withdraw", auth, withdraw);
-router.post("/transfer", auth, transfer);
+router.post("/deposit",  deposit);
+router.post("/withdraw",  withdraw);
+router.post("/transfer",  transfer);
 
 // Flutterwave webhook route (no auth needed as it's called by Flutterwave)
 router.post("/flutterwave-webhook", flutterwaveDeposit);
