b11c6b9fba65739d11b5ca1cdb1422795e3c4bd1
FredAbod <fredrickbolutife@gmail.com>
2025-03-05 12:09:43 +0100

    profile update endpoint

--- src/resources/user/routes/transaction.routes.js
+++ src/resources/user/routes/transaction.routes.js
@@ -1,10 +1,16 @@
-// write user routes logic here
 import express from "express";
 import { deposit, transfer, withdraw } from "../controllers/transaction.controller.js";
+import { flutterwaveDeposit } from "../controllers/flutterwaveTransaction.js";
+import { auth } from "../../../middleware/auth.js";
+
 const router = express.Router();
 
-router.post("/deposit", deposit);
-router.post("/withdraw", withdraw);
-router.post("/transfer", transfer);
+// Protected transaction routes
+router.post("/deposit", auth, deposit);
+router.post("/withdraw", auth, withdraw);
+router.post("/transfer", auth, transfer);
+
+// Flutterwave webhook route (no auth needed as it's called by Flutterwave)
+router.post("/flutterwave-webhook", flutterwaveDeposit);
 
 export default router;
--- src/resources/user/routes/user.routes.js
+++ src/resources/user/routes/user.routes.js
@@ -1,11 +1,24 @@
-// write user routes logic here
 import express from "express";
-import { addPhoneNumber, addUserName, signIn, signUp } from "../controllers/user.controller.js";
+import { 
+    signUp, 
+    signIn, 
+    addPhoneNumber, 
+    addUserName,
+    getProfile,
+    updateProfile
+} from "../controllers/user.controller.js";
+// import { auth } from "../../../middleware/auth.js";
+
 const router = express.Router();
 
+// Authentication routes
 router.post("/signup", signUp);
-router.post("/signIn", signIn);
-router.put("/add/:_id", addPhoneNumber);
-router.put("/addUsername/:_id", addUserName);
+router.post("/signin", signIn);
+
+// Routes requiring authentication
+router.put("/phone/:_id", addPhoneNumber);
+router.put("/username/:_id", addUserName);
+router.get("/profile/:id", getProfile);
+router.put("/profile/:id", updateProfile);
 
 export default router;
